ddfd2516a9baab9302a7903abea2fbac533af613
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2023-01-05 12:55:56 +0545

    Add files via upload

---
+++ day8/index.js
@@ -0,0 +1,33 @@
+const dayEL = document.getElementById("day")
+const hourEL = document.getElementById("hour")
+const minuteEL = document.getElementById("minute")
+const secondEL = document.getElementById("second")
+
+const newYearTime = new Date("Jan 1,2024 00:00:00").getTime()
+
+dayEL.addEventListener("", () => {
+
+})
+
+updateCountdown()
+function updateCountdown() {
+    const now = new Date().getTime()
+    const gap = newYearTime - now
+
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24
+
+    const d = Math.floor(gap / day)
+    const h = Math.floor((gap % day) / hour)
+    const m = Math.floor((gap % hour) / minute)
+    const s = Math.floor((gap % minute) / second)
+
+    dayEL.innerText = d
+    hourEL.innerText = h
+    minuteEL.innerText = m
+    secondEL.innerText = s
+
+    setTimeout(updateCountdown, 1000)
+}
