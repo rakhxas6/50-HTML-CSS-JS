48da1361eec848be638c7997faabbd4f1af0354a
Sudhan Kandel <sujan.kandel.0ne8@gmail.com>
2023-09-01 09:25:01 +0545

    Add files via upload

---
+++ Random color Genetator/index.js
@@ -0,0 +1,26 @@
+const containerEl = document.querySelector(".container");
+for (let index = 0; index < 30; index++) {
+  const colorContainerEl = document.createElement("div");
+  colorContainerEl.classList.add("color-container");
+  containerEl.appendChild(colorContainerEl);
+}
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
+  }
+  return colorCode;
+}
+
+const colorContainerEls = document.querySelectorAll(".color-container");
+generateColors();
+function generateColors() {
+  colorContainerEls.forEach((colorContainerEl) => {
+    const newColorCode = randomColor();
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorContainerEl.innerText = "#" + newColorCode
+  });
+}
